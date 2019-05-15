ef2d166b253893f9966b85597286e3102dc84751
Dharani Dhar Bhoi <dharanidhar.bhoi@factset.com>
2019-05-16 00:26:36 +0530

    commiting note book code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+      <div id="app">
+          <Notebook @change-page="changePage" @new-page="newPage" :pages="pages" :activePage="index" />
+          <Page @save-page="savePage" @delete-page="deletePage" :page="pages[index]" />
+      </div>
+    </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+    <script>
+    import Notebook from './components/Notebook'
+    import Page from './components/Page'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    export default {
+      name: 'app',
+      components: {
+        Notebook,
+        Page
+      },
+      data: () => ({
+        pages: [],
+        index: 0
+      }),
+      methods: {
+        newPage () {
+          this.pages.push({
+            title: '',
+            content: ''
+          })
+          this.index = this.pages.length - 1
+        },
+        changePage (index) {
+          this.index = index
+        },
+        savePage () {
+            const fs = require('fs')
+            var page = this.pages[this.index]
+            const content = page.content
+            try {
+              const data = fs.writeFileSync('C:/VueApplication/VueFirstDemo/electron_vue_example/'+ page.title+'.java', content)
+            } catch (error) {
+              console.error(error)
+            }            
+        },
+        deletePage () {
+          this.pages.splice(this.index, 1)
+          this.index = Math.max(this.index - 1, 0)
+        }
+      }
+    }
+    </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <style>
+    html, body, #app {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        display: flex;
+        flex-direction: row;
+    }
+    </style>
